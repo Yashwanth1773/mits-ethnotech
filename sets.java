9944f599d1837006f423426be50d87747a203259
Yash <tangellayashwanth@gmail.com>
2026-06-11 11:28:37 +0530

    first commit

---
+++ sets.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Set;
+import java.util.HashSet;
+public class sets{
+    public static void main(String[] args){
+        Set <Integer>s=new HashSet<>();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        System.out.println(s);
+    }
+}
